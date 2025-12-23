7beb56c7105785d42f98bbd78dea47f0fee03a80
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2025-12-23 20:16:50 +0900

    project upload

---
+++ src/main/java/com/amore/aketer/AketerApplication.java
@@ -0,0 +1,13 @@
+package com.amore.aketer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AketerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AketerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amore/aketer/AketerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amore.aketer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AketerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
